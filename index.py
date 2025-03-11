655ec04ea54a7285097d4b7b47b980d716ed4d19
Kimani145 <93636071+Kimani145@users.noreply.github.com>
2025-03-11 11:39:17 +0300

    New commit 1

--- index.py
+++ index.py
@@ -2,3 +2,15 @@ a=10
 b=20
 c=(a+b)
 print(c)
+def choose(fruit):
+    if fruit == "apple":
+        return "I love apples!"
+    elif fruit == "banana":
+        return "I love bananas!"
+    elif fruit == "orange":
+        return "I love oranges!"
+    else:
+        return "I don't like that fruit!"
+
+print(choose("apple"))
+
