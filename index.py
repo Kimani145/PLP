2e83796da27a3bce703fa8088ff2165475ee6b2b
Kimani145 <93636071+Kimani145@users.noreply.github.com>
2025-03-11 14:38:55 +0300

    First Python Assignment

---
+++ index.py
@@ -0,0 +1,9 @@
+def calculator(a, b):
+    c = a + b
+    print(f"Sum of two numbers is: {c}")
+    return c
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+calculator(a, b)
